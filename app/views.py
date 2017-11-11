a82127d01948a3f8cdd554097a987d23c22b46e7
Lesley Cordero <lesleycordero@Lesleys-MacBook-Pro.local>
2017-11-10 23:37:21 -0500

    replaced some sqlite

--- app/views.py
+++ app/views.py
@@ -144,3 +144,32 @@ def contact():
 							title='Submit a contact!', 
 							form=form))						
 
+
+# following code taken from server.py
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request.
+
+  The variable g is globally accessible.
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print("uh oh, problem connecting to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't, the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
--- db_functions.py
+++ db_functions.py
@@ -1,16 +1,31 @@
 import sqlite3
 import pandas as pd
+from sqlalchemy import * 
 
-db = sqlite3.connect("./database.db", check_same_thread=False)
-cursor = db.cursor()
+DATABASEURI = "postgresql://wke2102:8061@35.196.90.148/proj1part2"
+
+# make new account
+# query all projects
+# add event 
+# check the project leading the event (in charge of table)
+# creating a new workshop
+# check which person is hosting the workshop (host table)
+
+#db = sqlite3.connect("./database.db", check_same_thread=False)
+#cursor = db.cursor()
+
+engine = create_engine(DATABASEURI)
+cursor = engine.connect()
 
 def insert_company(name, address):	
 	cursor.execute(''' INSERT INTO companies (name, address, hires) VALUES (?, ?, ?) ''', (str(name), str(address), 0,))
 	db.commit()
 
+
 def insert_contact(name, number, email, company_id, position, notes):
 	cursor.execute(''' INSERT INTO contacts (name, number, email, company_id, position, notes) VALUES (?, ?, ?, ?, ?, ?) ''', (str(name), str(number), str(email), str(company_id), str(position), str(notes), ))
 	db.commit()
+
 
 def get_companies():
 	return list(cursor.execute(''' SELECT id, name FROM companies; '''))
